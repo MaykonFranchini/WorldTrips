3ec9456514a9f7d9ceb6c3c34ed5c9619cbdeb6d
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-09-11 06:11:57 +0100

    HTML document structure done

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
-import { generateKey } from "crypto";
+
 
 export const theme = extendTheme({
     colors: {
         gray: {
             "500": "#47585B"
         }
+    },
+    fonts: {
+        heading: 'Poppins',
+        body: 'Poppins',
     },
     styles: {
         global: {
